52260bb667cd462b69f4e576472216c9bfbe7316
GGUN <ddpgunhi23@gmail.com>
2020-10-07 00:29:32 +0800

    Updates

---
+++ precache-manifest.feef3ffdc0c4bb9bb5089b00faf81a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e585d4624cec196f1f90003e749f0a65",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "99923bf3422b01697079",
+    "url": "/movie_app_2020/static/css/main.610bfe47.chunk.css"
+  },
+  {
+    "revision": "dfeca280c2d05ba84fbc",
+    "url": "/movie_app_2020/static/js/2.f6053a57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.f6053a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99923bf3422b01697079",
+    "url": "/movie_app_2020/static/js/main.d9f7aae8.chunk.js"
+  },
+  {
+    "revision": "e2fcfd4cef567869b58f",
+    "url": "/movie_app_2020/static/js/runtime-main.38d2271a.js"
+  }
+]);
